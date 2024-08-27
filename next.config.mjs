3af302373a2112a0bfce9cd4b2693295e289e942
Cor Cronje <cor@phpweb.co.za>
2024-08-27 20:40:45 +0200

    [main] Add remote images sources

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "d3qdc2zh3mwabb.cloudfront.net",
+      "cdn.offerzen.com",
+      "cdn.jsdelivr.net",
+    ],
+  },
+};
 
 export default nextConfig;
