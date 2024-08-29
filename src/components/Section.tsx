34a198ba3af6eb3146de1b4042676e6d194bf049
Cor Cronje <cor@phpweb.co.za>
2024-08-29 16:00:44 +0200

    [main] Update section component by replading the card theme with a more minimalist indigo theme

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 function Section({title, children}) {
   return (
-    <section className="mb-5">
-      <h2 className="text-2xl mb-2">{title}</h2>
-      <div className="bg-gray-400 dark:bg-gray-600 rounded-md p-4">
+    <>
+      <section className="container mx-auto py-[5rem]">
+        <h2>
+          {title}
+        </h2>
         {children}
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 
