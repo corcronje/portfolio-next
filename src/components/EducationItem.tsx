9d4b90bee6e322a80b8ac6e2262c847447ae4ef7
Cor Cronje <cor@phpweb.co.za>
2024-08-29 15:59:43 +0200

    [main] Update education list and items with improved indigo theming

---
+++ src/components/EducationItem.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function EducationItem({title, institution, location, startDate, endDate}) {
+  return (
+    <article className="mb-5  mt-5 pt-5 border-t border-slate-200">
+      <h6>
+        {startDate} - {endDate}
+      </h6>
+      <h3>{title}</h3>
+      <div className="flex flex-row justify-between items-center text-slate-400 font-bold mb-2">
+        <small>{location}</small>
+        <small>{institution}</small>
+      </div>
+    </article>
+  );
+}
+
+export default EducationItem
---
+++ src/components/EducationStack.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EducationStack({children}) {
+  return (
+    <div>
+        {children}
+    </div>
+  )
+}
+
+export default EducationStack
