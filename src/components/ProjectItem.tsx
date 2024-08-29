db8c86bdf564a738a6b4d11bf0b2121e3f47910e
Cor Cronje <cor@phpweb.co.za>
2024-08-29 19:10:08 +0200

    [main] Add some margin to the heading

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ProjectItem({title, children}) {
   return (
     <article className="mt-5 pt-5 border-t border-slate-300">
-      <h3>{title}</h3>
+      <h3 className="mb-4">{title}</h3>
       {children}
     </article>
   );
