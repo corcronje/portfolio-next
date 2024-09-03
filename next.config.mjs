21e234b0438fc0a7a9eb756231517d1372c4b49a
Cor Cronje <cor@phpweb.co.za>
2024-09-03 03:12:12 +0200

    [main] Remove external image sources

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: [
-      "d3qdc2zh3mwabb.cloudfront.net",
-      "cdn.offerzen.com",
-      "cdn.jsdelivr.net",
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
